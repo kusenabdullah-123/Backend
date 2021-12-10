3b24affe4ad798642201fb489654210910a40aaf
Kusen Abdullah <kusena322@gmail.com>
2021-12-10 20:13:46 +0700

    tugas json web

---
+++ json/script.js
@@ -0,0 +1,276 @@
+const webRsnu = {
+  topbar: {
+    contact: [
+      {
+        judul: "Hubungi Kami",
+        url: "https://dev.rsnubanyuwangi.co.id/contact-us",
+      },
+      {
+        class: "fa fa-instagram",
+        isi: "rsnu.banyuwangi",
+        url: "https://www.instagram.com/rsnu.banyuwangi/",
+      },
+      {
+        class: "fa fa-phone",
+        isi: "33632965",
+        url: "tel:0333632965",
+      },
+    ],
+    sosmed: [
+      {
+        class: "fa fas-facebook",
+        url: "https://dev.rsnubanyuwangi.co.id/#facebook",
+      },
+      {
+        class: "fa fas-instagram",
+        url: "https://dev.rsnubanyuwangi.co.id/#instagram",
+      },
+    ],
+  },
+  header: {
+    judul: {
+      judul: "RSNU Banyuwangi",
+      url: "https://dev.rsnubanyuwangi.co.id",
+    },
+    navigasi: [
+      {
+        judul: "Beranda",
+        url: "https://dev.rsnubanyuwangi.co.id/",
+      },
+      {
+        judul: "Cari Dokter",
+        url: "https://dev.rsnubanyuwangi.co.id/",
+      },
+      {
+        judul: "Tentang RSNU",
+        url: "https://dev.rsnubanyuwangi.co.id/",
+      },
+      {
+        judul: "Layanan Unggulan",
+        dropdown: [
+          {
+            dropdownJudul: "Klinik Anak",
+            url: "https://dev.rsnubanyuwangi.co.id/klinik-anak",
+          },
+          {
+            dropdownJudul: "Klinik Bedah",
+            url: "https://dev.rsnubanyuwangi.co.id/klinik-anak",
+          },
+          {
+            dropdownJudul: "Klinik Tb",
+            url: "https://dev.rsnubanyuwangi.co.id/klinik-anak",
+          },
+        ],
+      },
+      {
+        judul: "Fasilitas Dan Layanan",
+        dropdown: [
+          {
+            dropdownJudul: "Radiologi",
+            url: "https://dev.rsnubanyuwangi.co.id/klinik-anak",
+          },
+          {
+            dropdownJudul: "UGD",
+            url: "https://dev.rsnubanyuwangi.co.id/klinik-anak",
+          },
+          {
+            dropdownJudul: "Farmasi",
+            url: "https://dev.rsnubanyuwangi.co.id/klinik-anak",
+          },
+        ],
+      },
+      {
+        judul: "Information Center",
+        dropdown: [
+          {
+            dropdownJudul: "Firektori Rumah Sakit",
+            url: "https://dev.rsnubanyuwangi.co.id/klinik-anak",
+          },
+          {
+            dropdownJudul: "PErusahaan Asuransi",
+            url: "https://dev.rsnubanyuwangi.co.id/klinik-anak",
+          },
+          {
+            dropdownJudul: "Jajaran Direksi",
+            url: "https://dev.rsnubanyuwangi.co.id/klinik-anak",
+          },
+        ],
+      },
+    ],
+  },
+  slider: [
+    {
+      imgUrl: "https://media.rsnubanyuwangi.co.id/slider/gallery-1.jpg",
+      desc: "RSNU Banyuwangi",
+    },
+    {
+      imgUrl: "https://media.rsnubanyuwangi.co.id/slider/gallery-1.jpg",
+      desc: "RSNU Banyuwangi",
+    },
+    {
+      imgUrl: "https://media.rsnubanyuwangi.co.id/slider/gallery-1.jpg",
+      desc: "RSNU Banyuwangi",
+    },
+  ],
+  section1: {
+    judul: "Layanan Unggulan",
+    desc: "Telusuri lebih dalam pelayanan unggulan yang dapat kami lakukan untuk Anda.",
+    kategori: [
+      {
+        judulKatergori: "Klinik Bayi Dan Anak",
+        desckategori:
+          "Layanan untuk kesehatan bayi dan anak, tumbuh kembang bayi, balita, dan pra-remaja secara komprehensif.",
+        image:
+          "https://media.rsnubanyuwangi.co.id/klinik/5e97bbce39a70_20200416085838-1.jpg",
+        url: "https://dev.rsnubanyuwangi.co.id/center-of-excellence",
+      },
+      {
+        judulKatergori: "Klinik Bedah",
+        desckategori:
+          "RSNU Banyuwangi membuka pelayanan baru yaitu Tuberculosis Directly Observed Treatment Short-course (TB DOTS)  dan Multi Drug Resistant Tuberculosis (MDR TB). RSNU Banyuwangi membuka pelayanan TB DOTS di wilayah Kabupaten Banyuwangi. ",
+        image:
+          "https://media.rsnubanyuwangi.co.id/klinik/5e97bbce39a70_20200416085838-1.jpg",
+        url: "https://dev.rsnubanyuwangi.co.id/center-of-excellence",
+      },
+      {
+        judulKatergori: "Klinik Tb Dots",
+        desckategori:
+          "Layanan untuk kesehatan bayi dan anak, tumbuh kembang bayi, balita, dan pra-remaja secara komprehensif.",
+        image:
+          "https://media.rsnubanyuwangi.co.id/klinik/5e97bbce39a70_20200416085838-1.jpg",
+        url: "https://dev.rsnubanyuwangi.co.id/center-of-excellence",
+      },
+    ],
+  },
+  section2: {
+    judul: "Fasilitias & Layanan",
+    desc: "Memberikan pelayanan terdepan dengan dukungan tenaga medis berkualitas, teknologi medis terkini, serta sistem informasi digital yang terintegrasi. Kenali lebih dalam di sini.",
+    listLayanan: [
+      {
+        judul: "Radiologi",
+        descLayanan:
+          "Kegiatan pelayanan radiologi di RSNU bertujuan untuk membantu klinisi dalam menilai adanya kelainan/lesi patologis yang mencakup seluruh bidang kedokteran, terutama bedah, penyakit dalam",
+      },
+      {
+        judul: "Ambulance",
+        descLayanan:
+          "Kegiatan pelayanan radiologi di RSNU bertujuan untuk membantu klinisi dalam menilai adanya kelainan/lesi patologis yang mencakup seluruh bidang kedokteran, terutama bedah, penyakit dalam",
+      },
+      {
+        judul: "IGD",
+        descLayanan:
+          "Kegiatan pelayanan radiologi di RSNU bertujuan untuk membantu klinisi dalam menilai adanya kelainan/lesi patologis yang mencakup seluruh bidang kedokteran, terutama bedah, penyakit dalam",
+      },
+    ],
+  },
+  section3: {
+    judul: "Penawaran Terbaik",
+    desc: "Temukan penawaran istimewa untuk Anda",
+    cards: [
+      {
+        image: "https://media.rsnubanyuwangi.co.id/artikel/promo1.jpg",
+        judulcard: "Promo Sepesial Screning",
+        url: "https://dev.rsnubanyuwangi.co.id/special-offer/promo-spesial-couple-fertility-screening",
+      },
+      {
+        image: "https://media.rsnubanyuwangi.co.id/artikel/promo1.jpg",
+        judulcard: "Promo Sepesial Screning",
+        url: "https://dev.rsnubanyuwangi.co.id/special-offer/promo-spesial-couple-fertility-screening",
+      },
+      {
+        image: "https://media.rsnubanyuwangi.co.id/artikel/promo1.jpg",
+        judulcard: "Promo Sepesial Screning",
+        url: "https://dev.rsnubanyuwangi.co.id/special-offer/promo-spesial-couple-fertility-screening",
+      },
+    ],
+    moreSpesial: {
+      url: "https://dev.rsnubanyuwangi.co.id/special-offer/",
+    },
+  },
+  section4: {
+    judul: "Kisah Inspiratif dari Para Pasien Kami",
+    quotes: [
+      {
+        nama: "Stenfany",
+        image: "https://media.rsnubanyuwangi.co.id/pasien/testimonials-4.jpg",
+        quote:
+          "Saat itu kaki dan tangan kanan saya tidak bisa digerakkan. Berkat penanganan yang sigap dan perawatan intensif di bawah pengawasan dokter di RS NU, kini sedikit-sedikit tangan saya sudah bisa digerakkan.",
+      },
+      {
+        nama: "Bapak Hadi",
+        image: "https://media.rsnubanyuwangi.co.id/pasien/testimonials-4.jpg",
+        quote:
+          "Saat itu kaki dan tangan kanan saya tidak bisa digerakkan. Berkat penanganan yang sigap dan perawatan intensif di bawah pengawasan dokter di RS NU, kini sedikit-sedikit tangan saya sudah bisa digerakkan.",
+      },
+      {
+        nama: "Bapak Priyanto",
+        image: "https://media.rsnubanyuwangi.co.id/pasien/testimonials-4.jpg",
+        quote:
+          "Saat itu kaki dan tangan kanan saya tidak bisa digerakkan. Berkat penanganan yang sigap dan perawatan intensif di bawah pengawasan dokter di RS NU, kini sedikit-sedikit tangan saya sudah bisa digerakkan.",
+      },
+    ],
+  },
+  section5: {
+    judul: "Berita dan Artikel Kesehatan",
+    cardArtikel: [
+      {
+        time: "Minggu, 14-11-2021",
+        judulArtikel: "Nyeri Panggul Tak Kunjung Usai? Telusuri Penyebabnya!",
+        descArtikel: "Nyeri Panggul Tak Kunjung Usai? Telusuri Penyebabnya!",
+        urlArtikel:
+          "https://dev.rsnubanyuwangi.co.id/news-articles/nyeri-punggung-tak-kunjung-usai",
+      },
+      {
+        time: "Selasa, 23-11-2021",
+        judulArtikel: "Kenali Jenis Penyakit Reumatik dan Penyebabnya",
+        descArtikel: "Kenali Jenis Penyakit Reumatik dan Penyebabnya",
+        urlArtikel:
+          "https://dev.rsnubanyuwangi.co.id/news-articles/nyeri-punggung-tak-kunjung-usai",
+      },
+      {
+        time: "Selasa, 23-11-2021",
+        judulArtikel: "Gangguan Tidur dan Cara Mengatasinya",
+        descArtikel: "Gangguan Tidur dan Cara Mengatasinya",
+        urlArtikel:
+          "https://dev.rsnubanyuwangi.co.id/news-articles/nyeri-punggung-tak-kunjung-usai",
+      },
+    ],
+  },
+  widget: [
+    {
+      nama: "Ambulance",
+      desc: "Jika Anda butuh layanan Ambulance, silakan hubungi nomor telepon di bawah ini",
+      noTelp: "0852-3468-5232",
+    },
+    {
+      nama: "Cari Dokter",
+      url: "https://dev.rsnubanyuwangi.co.id/find-doctor",
+    },
+  ],
+  footer: {
+    list: [
+      {
+        nama: "Beranda",
+        url: "https://dev.rsnubanyuwangi.co.id/",
+      },
+      {
+        nama: "Tentang RSNU",
+        url: "https://dev.rsnubanyuwangi.co.id/",
+      },
+      {
+        nama: "Perusahaan Asuransi Mitra",
+        url: "https://dev.rsnubanyuwangi.co.id/",
+      },
+      {
+        nama: "Hubungi Kami",
+        url: "https://dev.rsnubanyuwangi.co.id/",
+      },
+      {
+        nama: "Daftar Klinik",
+        url: "https://dev.rsnubanyuwangi.co.id/",
+      },
+    ],
+  },
+};
+document.write(JSON.stringify(webRsnu));
+s;
